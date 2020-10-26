c1e323d3129ccd3fd0dcfad128b6985ebdec5e43
Saiki-san <saiki_shinnya@yahoo.co.jp>
2020-10-26 16:13:59 +0900

    バリデーションの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+  #ログインしてるか否かを判定。ログインしていたら次のメソッドへ
   before_action :authenticate_user!
+  # devise_controlloerの有無を判定し、あった場合に次のメソッドを開始
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
