3c419b1baba9fb7477db1c369068f33b43085fbe
Saiki-san <saiki_shinnya@yahoo.co.jp>
2020-10-26 17:05:53 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'messages/index'
   root to: "messages#index"
+  # ログイン・ユーザー情報の編集・ログアウトのアクション
   resources :users, only: [:edit, :update, :destroy]
+  # チャットルームの作成アクション
+  resources :rooms, only: [:new, :create]
 end
