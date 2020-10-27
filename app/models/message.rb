1414f80f1b19e8d52acf27c10503f167788dfaef
Saiki-san <saiki_shinnya@yahoo.co.jp>
2020-10-27 13:17:05 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,6 +7,13 @@ class Message < ApplicationRecord
   # この記述により、「モデル.ファイル名」(例：Message.〜image)で、添付されたファイルにアクセスできるようになる
   # このとき、messagesテーブルにカラムを追加は不要。
   has_one_attached :image
-  # メッセージ(content)送信時にroomが空欄だった場合、作成できない様に設定。
-  validates :content, presence: true
+  # メッセージ(content)送信時にcontentが空欄だった場合、送信できない様に設定。
+  # unlessにより、was_attached?で画像が添付されてない状態(false)であれば、バリデーションで検証を行う仕様に。
+  # つまり、画像が添付されていれば、content無しでOK。画像が添付されていなければcontentが必要。(どちらか片方必須の制約)
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    # 画像があればtrue、なければfalseを返す
+    self.image.attached?
+  end
 end
